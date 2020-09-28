178c37a8fc89ce034607ba086b78669942744edf
RonaldKask <70570848+RonaldKask@users.noreply.github.com>
2020-09-28 23:39:14 +0300

    Add files via upload

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Test
 {
@@ -6,55 +7,84 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-
-            int randomNumber = rnd.Next(1, 4);
-
             string UserInput;
             string ComputerInput;
 
-            Console.WriteLine("Do you choose rock, paper or scissors?");
-            UserInput = Console.ReadLine().ToLower();
+            int UserScore = 0;
+            int ComputerScore = 0;
 
-            if (randomNumber == 1)
+            while (UserScore < 3 && ComputerScore < 3)
             {
-                ComputerInput = "rock";
-            }else if (randomNumber == 2)
+                Console.WriteLine("Do you choose rock, paper or scissors?");
+                UserInput = Console.ReadLine().ToLower();
+
+
+                Random rnd = new Random();
+
+                int randomNumber = rnd.Next(1, 4);
+
+                if (randomNumber == 1)
+                {
+                    ComputerInput = "rock";
+                }
+                else if (randomNumber == 2)
+                {
+                    ComputerInput = "paper";
+                }
+                else
+                {
+                    ComputerInput = "scissors";
+                }
+
+
+                if (UserInput == ComputerInput)
+                {
+                    Console.WriteLine($"It's a draw! The computer also chose {UserInput}");
+                }
+                else if (UserInput == "rock" && ComputerInput == "scissors")
+                {
+                    UserScore++;
+                    Console.WriteLine($"You got a point! The computer chose scissors. You now have {UserScore} points and the computer has {ComputerScore} points.");
+                }
+                else if (UserInput == "rock" && ComputerInput == "paper")
+                {
+                    ComputerScore++;
+                    Console.WriteLine($"You lost! The computer chose paper. You have {UserScore} points and the computer has {ComputerScore} points.");
+                }
+                else if (UserInput == "paper" && ComputerInput == "rock")
+                {
+                    UserScore++;
+                    Console.WriteLine($"You got a point! The computer chose rock. You now have {UserScore} points and the computer has {ComputerScore} points.");
+                }
+                else if (UserInput == "paper" && ComputerInput == "scissors")
+                {
+                    ComputerScore++;
+                    Console.WriteLine($"You lost! The computer chose scissors. You have {UserScore} points and the compter has {ComputerScore} points.");
+                }
+                else if (UserInput == "scissors" && ComputerInput == "paper")
+                {
+                    UserScore++;
+                    Console.WriteLine($"You got a point! The computer chose paper. You now have {UserScore} points and the computer has {ComputerScore} points.");
+                }
+                else if (UserInput == "scissors" && ComputerInput == "rock")
+                {
+                    ComputerScore++;
+                    Console.WriteLine($"You lost! The computer chose rock. You have {UserScore} points and the computer has {ComputerScore} points.");
+                }
+                else
+                {
+                    Console.WriteLine("You need to enter either rock, paper or scissors in order to play this game.");
+                }
+
+                }
+            if (UserScore == 3)
             {
-                ComputerInput = "paper";
-            }else
-            {
-                ComputerInput = "scissors";
+                Console.WriteLine("You won! The computer lives to fight another day.");
             }
-
-
-            if (UserInput == ComputerInput)
+            else
             {
-                Console.WriteLine($"It's a draw! The computer also chose {UserInput}");
-            } else if(UserInput == "rock" && ComputerInput == "scissors")
-            {
-                Console.WriteLine("You won! The computer chose Scissors.");
-            }else if (UserInput == "rock" && ComputerInput == "paper")
-            {
-                Console.WriteLine("You lost! The computer chose Paper.");
-            }else if (UserInput == "paper" && ComputerInput == "rock")
-            {
-                Console.WriteLine("You won! The computer chose Rock.");
-            }else if (UserInput == "paper" && ComputerInput == "scissors")
-            {
-                Console.WriteLine("You lost! The computer chose Scissors");
-            }else if (UserInput == "scissors" && ComputerInput == "paper")
-            {
-                Console.WriteLine("You won! The computer chose Paper.");
-            }else if (UserInput == "scissors" && ComputerInput == "rock")
-            {
-                Console.WriteLine("You lost! The computer chose Rock.");
-            }else
-            {
-                Console.WriteLine("You need to enter either Rock, Paper or Scissors in order to play this game.");
+                Console.WriteLine("The computer won. You live to fight another day.");
             }
-
-
-        }
-    }
+        }    
+    }  
 }
